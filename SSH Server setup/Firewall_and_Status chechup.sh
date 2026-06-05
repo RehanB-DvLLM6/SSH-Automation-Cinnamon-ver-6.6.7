06b608d6a5796102ecab085fa9a14ee75623edc2
Rehan Bhatti <cs.bhatti288@gmail.com>
2026-06-06 00:51:52 +0500

    Add firewall setup and status check script
    Added script to enable firewall and check status.

---
+++ SSH Server setup/Firewall_and_Status chechup.sh
@@ -0,0 +1,12 @@
+//ENABLE FIREWALL
+pseudo@IdeaPad-Gaming-3-15ARH05:~$ sudo ufw enable
+Firewall is active and enabled on system startup
+//STATUS REVIEW
+pseudo@IdeaPad-Gaming-3-15ARH05:~$ sudo ufw status
+Status: active
+To                         Action      From
+--                         ------      ----
+22/tcp                     ALLOW       Anywhere
+22/tcp (v6)                ALLOW       Anywhere (v6)
+
+pseudo@IdeaPad-Gaming-3-15ARH05:~$
